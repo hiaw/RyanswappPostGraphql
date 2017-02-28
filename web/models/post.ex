60e1f47b9711f31dabe99aaf338efcef1128c859
Daniel Chong <danielgq.chong@gmail.com>
2017-02-28 17:08:04 +1300

    added post model

---
+++ web/models/post.ex
@@ -0,0 +1,19 @@
+defmodule RyanswappPostGraphql.Post do
+  use RyanswappPostGraphql.Web, :model
+
+  schema "posts" do
+    field :title, :string
+    belongs_to :user, RyanswappPostGraphql.User
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
+end
